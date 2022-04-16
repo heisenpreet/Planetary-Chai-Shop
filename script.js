5916a10417c362ebf1aafb2099fab9eff6be04ee
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-04-16 20:18:28 +0530

    v2

--- script.js
+++ script.js
@@ -15,7 +15,32 @@ const overlay = document.querySelector(".overlay");
 const popup = document.querySelector(".popup");
 const popupleft = document.querySelector(".popup-left");
 const popupbtn = document.querySelector(".popup__btn");
+const timeleft = document.querySelector(".Welcome-msg__timeleft");
+const Welcome = document.querySelector(".Welcome-msg");
 
+const msg = function () {
+  let timer = 5;
+  const startTimer = () => {
+    setInterval(() => {
+      if (timer > 0) {
+        timer--;
+        timeleft.textContent = String(timer).padStart(2, 0);
+      }
+    }, 1000);
+  };
+
+  setTimeout(() => {
+    startTimer();
+  }, 1500);
+
+  setTimeout(() => {
+    overlay.classList.toggle("hidden");
+    Welcome.classList.toggle("hidden");
+    body.classList.toggle("inactive");
+  }, 8000);
+};
+
+msg();
 const showClose = function () {
   body.classList.toggle("inactive");
   overlay.classList.toggle("hidden");
