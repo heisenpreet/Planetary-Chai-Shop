076a598d7d6ac0edd2da7a719e1da695231134ec
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-04-20 19:53:52 +0530

    v2

--- script.js
+++ script.js
@@ -33,6 +33,13 @@ month.textContent = new Intl.DateTimeFormat(navigator.language, {
 
 let messageTimer;
 const welcomeMsg = function () {
+  // This prevents the page from scrolling down to where it was previously.
+  if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+  }
+  // This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
+  window.scrollTo(0, 0);
+
   let timer = 10;
   const startTimer = () => {
     setInterval(() => {
